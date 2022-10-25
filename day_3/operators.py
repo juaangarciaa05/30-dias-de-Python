d1c3bc14ee896209c0c4e634c3e0b147c1ba2c81
juaangarciaa05 <jgarlan1208@m.educaand.es>
2022-10-25 13:32:27 +0200

    Refactor

---
+++ day_3/operators.py
@@ -0,0 +1,18 @@
+age = 17
+
+height = float(175)
+
+c_number = complex(2, 5)
+
+print("Area of the triangle:", 0.5 * int(input('Base: ')) * int(input('Height: ')))
+
+print("Perimeter of the triangle:", int(input('a: ')) + int(input('b: ')) + int(input('c: ')))
+
+length = int(input('Enter length: '))
+width = int(input('Enter width: '))
+print('Area: ', length * width)
+print('Perimeter: ', 2 * (length + width))
+
+radius = int(input('Enter radius: '))
+print('Area:', 3.14 * radius * radius)
+print('Circunference', 2 * 3.14 * radius)
