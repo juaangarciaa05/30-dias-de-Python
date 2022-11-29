ca687bb559cc3cb81e89be848d173f9cb231b467
juaangarciaa05 <l>
2022-11-29 12:58:15 +0100

    Refactor

--- day_3/operators.py
+++ day_3/operators.py
@@ -34,3 +34,28 @@ print(2 if 2 < (y2 - y1) / (x2 - x1) else (y2 - y1) / (x2 - x1))
 for x in range(0, 10):
     print(x ** 2 + 6 * x + 9)
 print(3, -3, "is where y is 0")
+
+print(not len('python') == len('dragon'))
+
+print('on' in 'python' and 'on' in 'dragon')
+
+print('jargon' in "I hope this course is not full of jargon")
+
+print('on' not in 'python' and 'on' in 'dragon')
+
+print(str(float(len('python'))))
+
+number = int(input('Enter number:'))
+print("Even" if number % 2 == 0 else "Odd")
+
+print(type('10') == type(10))
+
+hours = int(input('Enter hours:'))
+rph = int(input('Enter rate per hour:'))
+print("Weekly Earning:", hours * rph)
+
+years = int(input('Enter years:'))
+print(years * 365 * 24 * 60 * 60 * 60)
+
+for i in range(1, 6):
+    print(i, i ** 0, i ** 1, i ** 2, i ** 3)
