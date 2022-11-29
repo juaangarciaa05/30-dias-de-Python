f930f0537498c6958db754c45fd15103a72555f6
juaangarciaa05 <l>
2022-11-29 13:32:06 +0100

    Refactor

---
+++ day_4/strings.py
@@ -0,0 +1,30 @@
+a= "Thirty"
+b= "days"
+c= "of"
+d= "Python"
+space= " "
+print(a+space+b+space+c+space+d)
+
+e= "Coding"
+f= "for"
+g= "all"
+print(e+space+f+space+g)
+
+company= "Coding for all"
+
+print(company)
+
+print(len(company))
+
+print(company.upper())
+
+print(company.lower())
+
+print(company.capitalize())
+
+print(company.title())
+
+print(company.swapcase())
+
+
+
