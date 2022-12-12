639a085748e0f930a36e6fd12c73d5855f53cfce
juaangarciaa05 <l>
2022-12-12 12:44:31 +0100

    Refactor

---
+++ day_8/dictionaries.py
@@ -0,0 +1,9 @@
+dog= dict()
+
+dog= {"name","color","breed","legs","age"}
+
+student_dictionary= dict()
+student_dictionary= {"first_name":"Juan", "last_name":"García", "gender":"M", "marital_status":"unmarried", "skills":"music", "country":"Spain", "city":"Jerez","adress":"Chapín"}
+
+print(len(student_dictionary))
+
