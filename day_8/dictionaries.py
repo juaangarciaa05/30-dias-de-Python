c30e639075045a818e8eb79d46715b67a3b95c19
juaangarciaa05 <l>
2022-12-20 13:42:39 +0100

    Refactor

--- day_8/dictionaries.py
+++ day_8/dictionaries.py
@@ -2,10 +2,34 @@
 
 dog= dict()
 
-dog= {"name","color","breed","legs","age"}
+dog= {"name":"Rocky","color":"golden","breed":"golden retriever","legs":"four","age":"2 years"}
 
-student_dictionary= dict()
-student_dictionary= {"first_name":"Juan", "last_name":"García", "gender":"M", "marital_status":"unmarried", "skills":"music", "country":"Spain", "city":"Jerez","adress":"Chapín"}
+student_dictionary = {
+"first_name":"Juan",
+"last_name":"García", 
+"gender":"M", 
+"marital_status":"unmarried", 
+"skills":["music"], 
+"country":"Spain", 
+"city":"Jerez",
+"address":"Chapín"
+}
 
 print(len(student_dictionary))
+
+print(student_dictionary["skills"])
+print(type(student_dictionary["skills"]))
+
+student_dictionary["skills"].append("computing")
+student_dictionary["skills"].append("swimming")
+
+keys = student_dictionary.keys()
+
+values = student_dictionary.values()
+
+items = student_dictionary.items()
+
+del student_dictionary['gender']
+
+del student_dictionary
 
